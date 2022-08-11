e8505e9c73bc4a5013a5738dfcba1662cbd8dae0
joaquin achacollo <joaquinachacollo@gmail.com>
2022-08-10 21:07:02 -0300

    2-PF

--- script.js
+++ script.js
@@ -1,68 +1,79 @@
-let busqueda
-let opcion
+let d = document
+let resultado
+let busqueda = d.getElementById("busqueda")
+let menu = d.getElementById("menu")
+let containerCatalogo = d.getElementById("peliculas")
 let peliculas = [
-    {nombre: "juego del miedo", genero: "(terror)", top: 4},
-    {nombre: "alladin", genero: "(infantil)", top: 10},
-    {nombre: "mision imposible", genero: "(accion)", top: 6},
-    {nombre: "un sueño posible", genero: "(drama)", top: 7},
-    {nombre: "gladiador", genero: "(aventura)", top: 5},
-    {nombre: "venom", genero: "(ciencia ficcion)", top: 2},
-    {nombre: "eternals", genero: "(ciencia ficcion)", top: 8},
-    {nombre: "resident evil", genero: "(terror)", top: 9},
-    {nombre: "nadie", genero: "(accion)", top: 3},
-    {nombre: "star wars", genero: "(ciencia ficcion)", top: 1}
+    {id: 1,nombre: "juego del miedo", genero: "terror", top: 4},
+    {id: 2,nombre: "alladin", genero: "infantil", top: 10},
+    {id: 3,nombre: "mision imposible", genero: "accion", top: 6},
+    {id: 4,nombre: "un sueño posible", genero: "drama", top: 7},
+    {id: 5,nombre: "gladiador", genero: "aventura", top: 5},
+    {id: 6,nombre: "venom", genero: "ciencia ficcion", top: 2},
+    {id: 7,nombre: "eternals", genero: "ciencia ficcion", top: 8},
+    {id: 8,nombre: "resident evil", genero: "terror", top: 9},
+    {id: 9,nombre: "nadie", genero: "accion", top: 3},
+    {id: 10,nombre: "star wars", genero: "ciencia ficcion", top: 1}
 ]
 
-///////////////////// MAIN /////////////////////////
+//////////////////////// MAIN ////////////////////////////
 
-do {
-    mostrarMenu()
-    opcion = parseInt(prompt("ingrese su eleccion")) 
-} while (opcion < 1 || opcion > 4);
+let peliculasJSON = JSON.stringify(peliculas)
+localStorage.setItem("peliculas", peliculasJSON)
 
+peliculas = JSON.parse(peliculasJSON)
 
-if (opcion === 1) {
-    mostrarTop()
-}
-else if (opcion === 2) {
-    mostrarGenero()
-}
-else if (opcion === 3) {
-    busqueda = prompt("ingrese el nombre de la pelicula que esta buscando: ")
-    let resultado = peliculas.find((pelis) => pelis.nombre == busqueda)
-    console.log("Nombre: " + resultado.nombre + " Genero: " + resultado.genero + " Top: " + resultado.top)
-}
-else if (opcion === 4) {
-    console.log("adios nos vemos")
-}
+let catologo = d.getElementById("catalogo")
+catologo.addEventListener("click",mostrarCatalogo)
 
 
-///////////////// FUNCIONES ////////////////////////
 
-function mostrarGenero() {
-    for (let i = 0; i < peliculas.length; i++) {
-        console.log("Genero: " + peliculas[i].genero + " Nombre: " + peliculas[i].nombre)
+
+let bus = d.createElement("div")
+bus.innerHTML = `
+<input type= text id= "buscar" input>
+`
+busqueda.append(bus)
+
+let buscar = d.getElementById("buscar")
+buscar.addEventListener("change", busquedaP)
+
+
+//////////////////////// FUNCIONES ////////////////////////////
+
+function busquedaP(e) {
+    containerCatalogo.innerHTML = ""
+    resultado = peliculas.find(pelicula => pelicula.nombre == e.target.value)
+    let resultadoFinal = d.createElement("div")
+    resultadoFinal.className = "pelicula"
+    resultadoFinal.innerHTML = `
+    <p>Nombre: ${resultado.nombre}</p>
+    <p>Genero: ${resultado.genero}</p>
+    <p>Top: ${resultado.top}</p>
+    <br>
+    `
+    
+    containerCatalogo.append(resultadoFinal)
+}    
+
+function mostrarCatalogo() {
+    containerCatalogo.innerHTML = ""
+    peliculas.sort(function(a, b) {
+        return a.top - b.top
+    })
+    for (const peli of peliculas) {
+        let listadoPeli = d.createElement("div")
+        listadoPeli.className = "pelicula"
+        listadoPeli.innerHTML = `
+        <p>Nombre: ${peli.nombre}</p>
+        <p>Genero: ${peli.genero}</p>
+        <p>Top: ${peli.top}</p>
+        <br>
+        `
+        containerCatalogo.append(listadoPeli)
     }
 }
 
-function mostrarTop() {
-    peliculas.sort((a,b) => a.top - b.top)
-    for (let i = 0; i < peliculas.length; i++) {
-        console.log("Top: " + peliculas[i].top + " Nombre: " + peliculas[i].nombre)
-    }
-}
 
-
-function mostrarMenu() {
-    console.log("-------------------------------------------------------")
-    console.log("-------------------------------------------------------")
-    console.log("-------------------------------------------------------")
-    console.log("presione (1) si quiere ver el top de las peliculas")
-    console.log("presione (2) si quiere ver los generos de peliculas")
-    console.log("presione (3) si quiere ingresar el nombre de la pelicula")
-    console.log("presione (4) para salir")
-    console.log("-------------------------------------------------------")
-    console.log("-------------------------------------------------------")
-}
 
 
